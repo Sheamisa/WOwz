95905df850bf60771e50c3931ecc02d6fdccc078
deadwars3232 <99791402+deadwars3232@users.noreply.github.com>
2024-01-09 20:28:43 +0800

    result_embed.py

--- result_embed.py
+++ result_embed.py
@@ -209,7 +209,7 @@ def result():
                             "url": thumbnail_url,
                         },
                         "author": {
-                            "name": "Test Mgui - Results", 
+                            "name": "METACORE MGUI", 
                             "url": "", 
                         },
                         "color": 0x000d21,
